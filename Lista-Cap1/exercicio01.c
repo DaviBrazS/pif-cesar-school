7879b3974487318b0325d4d0fb728c0c12fd5039
DaviBrazS <davibrazemp@gmail.com>
2026-08-19 21:40:40 -0300

    Adiciona exercícios de variáveis e printf - Lista Cap1

---
+++ Lista-Cap1/exercicio01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int ano_letivo = 2026;
+    printf("O ano letivo corrente é: %d\n", ano_letivo);
+
+    return 0;
+}
---
+++ Lista-Cap1/exercicio02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    float Euler = 2.71828;
+    printf("O valor de Euler é: %.3f\n", Euler);
+
+    return 0;
+}
